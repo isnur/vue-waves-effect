213419037cddc2ca9c413b88e3bd053782300c4e
Isnur Margono <isnur.margono@shopee.com>
2020-10-30 04:00:53 +0700

    fix build

--- build-all.js
+++ build-all.js
@@ -9,19 +9,15 @@ const DOCS_FOLDER = 'docs';
     try {
         // const indexPath = path.join('src', 'index.js');
         // build lib
-        await exec(`npm run dist`);
+        let { stdout, stderr } = await exec(`npm run dist`);
+        // if (stderr) {
+        //     console.log(stderr);
+        // }
 
         // build site
         await fs.emptyDir(DOCS_FOLDER); // empty the <docs> folder
 
-        console.log('dir from: ' + process.cwd());
-        let { stdout: out, stderr: err } = await exec(`cd ./site`);
-        console.log(err, out);
-        if (err) {
-            console.log(out);
-        }
-        console.log('dir to: ' + process.cwd());
-        let { stdout: sout, stderr: serr } = await exec(`npm run build`);
+        let { stdout: sout, stderr: serr } = await exec(`cd ${path.join(__dirname, 'site')} && npm run build --scripts-prepend-node-path=true`);
         console.log(serr, sout);
         if (serr) {
             console.log(sout);
