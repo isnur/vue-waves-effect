e9a6d4845634b318c706301f2f853503ced42d5e
Isnur Margono <isnur.margono@shopee.com>
2021-10-14 03:45:30 +0700

    fix site content

--- site/src/views/usage.vue
+++ site/src/views/usage.vue
@@ -1,33 +1,33 @@
 <template>
     <main class="usage">
         <h3>Usage</h3>
-                
+
         <h4><strong>Basic</strong></h4>
 
         <iframe width="100%" height="175" src="//jsfiddle.net/isnur/zp02fa3o/embedded/result,html,js/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
-        
+
         <h4><strong>With argument</strong></h4>
-     
+
         <h5>Hover</h5>
         The ripple will be seen when on hover.
         <iframe width="100%" height="215" src="//jsfiddle.net//isnur/frzemxLq/embedded/result,html,js/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
-     
+
         <h4><strong>With modifiers</strong></h4>
 
         <h5>Shake</h5>
         <iframe width="100%" height="220" src="//jsfiddle.net//isnur/y5h7g93v/embedded/result,html,js/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
-  
+
         <h5>Pressed</h5>
         <iframe width="100%" height="175" src="//jsfiddle.net//isnur/6gxq428a/embedded/result,html,js/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
-        
+
         <h5>Buttons</h5>
         <iframe width="100%" height="265" src="//jsfiddle.net//isnur/sntr0xz7/embedded/result,html,js/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
-        
+
         <h5>Icons</h5>
         <iframe width="100%" height="265" src="//jsfiddle.net//isnur/mLgqswpk/embedded/result,html,js/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
-        
+
         <h5>DIVs & Images</h5>
         <iframe width="100%" height="560" src="//jsfiddle.net//isnur/zt9d75jc/embedded/result,html,js/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
-        <div class="clearfix" style="margin-bottom: 30px;"></div>
+        <div class="clearfix"></div>
     </main>
 </template>
